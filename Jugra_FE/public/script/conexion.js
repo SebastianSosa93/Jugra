c3bb01ded1f9edd601a2ca71a80e2f43d06fa939
Sebastian <sebas.sosa2@gmail.com>
2025-06-18 22:51:52 -0300

    configurando el front

--- Jugra_FE/public/script/conexion.js
+++ Jugra_FE/public/script/conexion.js
@@ -1,6 +1,6 @@
 const conexion = async () => {
     try {
-        const res = await fetch('https://localhost:8443/config');
+        const res = await fetch('https://localhost:3000/config');
         const config = await res.json();
         return { puerto: config.puerto, servidor: config.servidor };
     } catch (error) {
