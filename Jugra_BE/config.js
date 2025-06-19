8c6d5f74d54653a3cbc44cc7fb641882dd0c0acb
Sebastian <sebas.sosa2@gmail.com>
2025-06-18 23:59:30 -0300

    arreglos en config

--- Jugra_BE/config.js
+++ Jugra_BE/config.js
@@ -11,7 +11,7 @@ const SECRET_KEY = process.env.JWT_SECRET;
 const adminClave = process.env.ADMIN_CLAVE;
 const adminEmail = process.env.ADMIN_EMAIL;
 const REFRESH_SECRET_KEY = process.env.RSK;
-const CORSORIGIN = process.env.SERVIDORFRONT;
+const CORSORIGIN = process.env.SERVIDORBACKEND;
 
 
 module.exports = {usr,pwd,port,servidorFront,servidorBack,SECRET_KEY,adminClave,adminEmail,REFRESH_SECRET_KEY,CORSORIGIN};
